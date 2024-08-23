06df0c28124fffbf9a9306323f848ad3bf88bfdf
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-23 06:55:26 -0700

    make counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import { useState } from "react";
 
+
+
+
+
+const App = ()=>{
+  let [num,setNum] = useState(0)
+
+  const counterPlus = ()=>{
+    setNum(num + 1)
+  }
+
+  return (
+    <>
+        <h1>hello World</h1>
+        <p>{num}</p>
+        <button onClick={counterPlus}>click</button>
+    </>
+
+  )
+}
+
+export default App;
