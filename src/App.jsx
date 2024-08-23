33f32681d3434fffad90a60c28a4b7b9ff184204
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-23 07:10:51 -0700

    counter app done

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,19 @@ const App = ()=>{
   const counterPlus = ()=>{
     setNum(num + 1)
   }
+  const counterMinus = ()=>{
+    setNum(num -1)
+  }
 
   return (
-    <>
-        <h1>hello World</h1>
-        <p>{num}</p>
-        <button onClick={counterPlus}>click</button>
-    </>
+    <div className="text-center">
+        <h1 className="text-center mt-3">Counter App</h1>
+        <p className="text-center mt-3 fs-1">{num}</p>
+    <div>
+        <button  className="btn btn-primary mx-3 mb-3 px-5 py-2" onClick={counterPlus}>Counter Add</button>
+        <button className="btn btn-danger  mb-3 px-5 py-2" onClick={counterMinus}>Counter Minus</button>
+    </div>
+</div>
 
   )
 }
