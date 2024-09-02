46979435709e1cab86a8cbb852ad498664f29346
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-02 09:10:04 -0700

    "Updated index.html to add Font Awesome stylesheet and modified App.jsx to redesign counter buttons with Font Awesome icons and custom styles."

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,54 @@ const App = ()=>{
   return (
     <div className="text-center">
         <h1 className="text-center mt-3">Counter App</h1>
-        <p className="text-center mt-3 fs-1">{num}</p>
+        
     <div>
-        <button  className="btn btn-success mx-3 mb-3 px-5 py-2" onClick={counterPlus}>Counter Add</button>
-        <button className="btn btn-warning mx-3 mb-3 px-5 py-2" onClick={counterReset}>Counter Reset</button>
-        <button className="btn btn-danger  mb-3 px-5 py-2" onClick={counterMinus}>Counter Minus</button>
+
+       {/* plus  */}
+    <div className="container d-flex justify-content-center p-3 text-center bg-dark">
+    <div className="btn-1">
+    <button
+  style={{
+    borderRadius: '50%',
+    width: '100px',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }} className="btn btn-success mx-3 mb-3 px-0 py-0" onClick={counterPlus}><i className="fa-solid fa-plus"></i></button>
+       
+    </div>
+         
+      <div className="para">
+      <p className="text-center text-light mt-3 fs-1">{num}</p>
+      </div>
+
+         <div className="btn-2">
+          {/* minus  */}
+
+         <button
+  style={{
+    borderRadius: '50%',
+    width: '100px',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }} className="btn btn-danger mx-3 mb-3 px-0 py-0" onClick={counterMinus}><i className="fa-solid fa-minus"></i></button>
+
+         </div>
+    </div>
+
+  {/* reset  */}
+  <button
+  style={{
+    borderRadius: '50%',
+    width: '100px',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }} className="btn btn-warning mx-3 mb-3 px-0 py-0" onClick={counterReset}>Reset</button> 
     </div>
 </div>
 
@@ -35,3 +78,7 @@ const App = ()=>{
 }
 
 export default App;
+
+
+
+
